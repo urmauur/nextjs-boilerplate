bd634928c26ce4377b031b0efc5e31760d7f4ca2
Faisal Amir <urmauur@gmail.com>
2024-10-16 20:25:45 +0700

    style: configure scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
-import './globals.css';
+import '@/styles/main.scss';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-sm bg-black text-white`}
       >
         {children}
       </body>
